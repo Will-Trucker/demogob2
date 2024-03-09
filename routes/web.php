ff978c103a25d7b3ef51652db532cb7d1bdbc957
Will-Trucker <willitomarley@outlook.es>
2024-03-09 06:50:09 -0600

    Modulo2-demorutas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('demo');
 });
+
+Route::get('/alcaldia',function(){
+    return view('alcaldia');
+});
+
+Route::get('/departamento',function(){
+    return view('departamento');
+});
+
